151894ba2c31398b8f503b12325f8cc43a873f78
Mike Giancola <mjgiancola@wpi.edu>
2016-09-21 11:48:22 +0300

    Updated comments

--- banks_analytics/c_test.py
+++ banks_analytics/c_test.py
@@ -1,7 +1,16 @@
+################################### PREAMBLE ###################################
+
 import subprocess
 import numpy as np
 
 from export_test import *
+
+# USAGE: This script runs multinomial_logistic_regression.py on the same seed
+#        with different C values to compare them. The vals array below contains
+#        the C values that the script will iterate over. Change it to include
+#        whichever values you want.
+
+#################################### SCRIPT ####################################
 
 # Array of C values to run model on
 vals = np.array([ 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.75, 0.9, 0.95, 1.0 ])
