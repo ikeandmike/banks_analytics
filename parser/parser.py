a1e46278d0fc77b3eec6af010a5813911027e02f
jabortell <jacobbortell@gmail.com>
2016-09-21 12:41:13 +0300

    Parser can now use CBR and select columns, but no bangs or divisions.

--- parser/parser.py
+++ parser/parser.py
@@ -32,13 +32,16 @@ group_revoked.add_argument("-rr", "--redownload-revoked", action="store_true",
 parser.add_argument("-s", "--select", metavar="COLUMNS", nargs='*',
                     help="Select columns from indicator dataset to use in model. To receive binary indicators (ie. is indicator in acceptable range) add a bang after the indicator (ie. 'N1!'). To receive ratios of indicators, specify with a slash (ie. 'N1/N2').")
 
+parser.add_argument("-cbr", action="store_true",
+                    help="Use CBR indicator dataset instead of banki.ru")
+
 args = parser.parse_args()
 
 ###############################
 ## Load External Files
 ###############################
 
-execfile("../banks_analytics/dictionaries.py")
+execfile("dictionaries.py")
 execfile("load_banki.py")
 execfile("load_banki_revoked.py")
 execfile("merge_banki_revoked.py")
@@ -52,10 +55,9 @@ new_ratios = {}
 # Collect requests for "Is indicator within its range?"
 new_bin_ranges = []
 
-
 # If there were select-column arguments given,
 # check if there is a request for division or range calculation.
-if not args.select == None:
+if not args.select == None and not args.cbr:
         # Helper list
         select = args.select[:]
         for i in args.select:
@@ -99,6 +101,8 @@ if not args.select == None:
                         pass 
         
 # If no columns were specified, get all the columns.
+elif args.cbr:
+        select = args.select[:]
 else:
     select = ind_dict_banki_ru()['ind_name']
     
@@ -108,6 +112,14 @@ else:
 
 # If there are no update or redownload requests, and the file exists,
 # just read in the local file.
+
+if args.cbr:
+        if os.path.isfile("../csv/cbr_standards_complete.csv"):
+                cbr = pd.read_csv("../csv/cbr_standards_complete.csv", index_col=False)
+        else:
+                print "Local CBR file not found. Exiting..."
+                SystemExit(0)
+
 if not (args.update_banki or args.redownload_banki or args.update_revoked
         or args.redownload_revoked) and os.path.isfile("../csv/banki_complete.csv"):
     # Read local csv
@@ -136,9 +148,16 @@ select = list(set(select))
 columns = select + new_bin_ranges
 columns = list(set(columns))
 
+print columns
+
 # Now we're ready to modify and cleanup the final dataset.
 # Here, we add our singular columns and special requests to the table.
-model_data = banki[['lic_num', 'period', 'months'] + columns]
+
+# TODO: Generalize this
+if args.cbr:
+        model_data = cbr[['lic_num', 'period', 'months'] + columns]
+else:
+        model_data = banki[['lic_num', 'period', 'months'] + columns]
 
 # Add new column binary classifiers to model_data.csv.
 for col in new_bin_ranges:
